8be71e7737524298273fa06e10b2318ab6f9fa28
Janus Gudjonsson <janusgudjonsson@gmail.com>
2021-07-19 00:08:20 +0000

    Wored on code related to ai animation

---
+++ Source/MyrkurProject/ThrowNotifyState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "ThrowNotifyState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYRKURPROJECT_API UThrowNotifyState : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
