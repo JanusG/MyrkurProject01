7ca2334896b64e73e6a02ebd286dc986b1c2c929
JanusKisil <janusgudjonssongmail.com>
2021-08-26 18:04:57 +0000

    worked on end game HUD

--- Source/MyrkurProject/Private/HUD/EndScreen.cpp
+++ Source/MyrkurProject/Private/HUD/EndScreen.cpp
@@ -2,4 +2,20 @@
 
 
 #include "HUD/EndScreen.h"
+#include "MyrkurProjectGameMode.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
 
+void UEndScreen::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+    AMyrkurProjectGameMode* GameMode = Cast<AMyrkurProjectGameMode>(GetWorld()->GetAuthGameMode());
+
+    MainMenu->OnClicked.AddDynamic(this, &UEndScreen::MenuClicked);
+}
+
+void UEndScreen::MenuClicked()
+{
+
+}
--- Source/MyrkurProject/Public/HUD/EndScreen.h
+++ Source/MyrkurProject/Public/HUD/EndScreen.h
@@ -6,6 +6,9 @@
 #include "Blueprint/UserWidget.h"
 #include "EndScreen.generated.h"
 
+class UTextBlock;
+class UButton;
+
 /**
  * 
  */
@@ -13,5 +16,19 @@ UCLASS()
 class MYRKURPROJECT_API UEndScreen : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+protected:
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UTextBlock* Winner;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* MainMenu;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* Restart;
+
+	virtual void NativeConstruct() override;
+
+	void MenuClicked();
 };
